63c18f136fc112d896a448e7132fe19f9b38ddeb
Afrah Hassan <afrah.hassan12345@gmail.com>
2020-02-25 20:44:46 +0200

    Initial commit- models and dbcontext

---
+++ Models/Custumer.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Chart.Api.Models
+{
+    public class Customer
+    {
+        public int Id { get; set ;}
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String State { get; set ;}
+    }
+}
---
+++ Models/Shipper.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Chart.Api.Models
+{
+    public class Shipper
+    { 
+        public int Id { get; set ;}
+        public String Name { get; set; }
+        public bool IsOnline { get;set; }
+    }
+}
